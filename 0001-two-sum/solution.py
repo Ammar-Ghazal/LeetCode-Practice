fc68176de41772996ca65d554b4d3ece06d573d2
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2023-01-28 01:14:47 +0000

    Sync LeetCode submission Runtime - 8706 ms (5.00%), Memory - 14.9 MB (100.00%)

---
+++ 0001-two-sum/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        listsize = len(nums)
+        for i in range(listsize):
+            for j in range(listsize):
+                if(i == j):
+                    pass
+                else:
+                    if(nums[i]+nums[j] == target):
+                        return[i,j]
