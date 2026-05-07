ba45b964a5e86381db4537f86fce3d2092049729
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-07 22:42:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.3 MB (61.46%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,22 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # O(n^2) time complexity
+        # O(n^2) time complexity, O(1) space complexity
         # length = len(nums)
         # for i in range(length):
-        #     for j in range(i+1,length):
+        #     for j in range(i+1, length):
         #         if i != j:
         #             if nums[i]+nums[j] == target:
         #                 return [i,j]
+        
+        # O(n) time complexity, O(n) space complexity (in the worst case that every value need be stored in the hashmap)
+        # contains all previous numbers that we went over
+        prevMap = {}
 
-        # O(n) time complexity
-        prevMap = {} # dict that contains all previous numbers that we went over
-
-        for i, n in enumerate (nums):
+        for i, n in enumerate(nums):
             diff = target - n
+            #we put the numbers in the keys so we can perform this check in O(1) time
             if diff in prevMap:
-                return [prevMap[diff],i] 
+                return [prevMap[diff], i]
             prevMap[n] = i
         return
-            
+
+
 
 
