26a62bd8febefc412a62c4856f58c730cbea93e7
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-08 00:50:33 +0000

    Sync LeetCode submission Runtime - 42 ms (68.90%), Memory - 28.7 MB (42.62%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,19 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # sliding window problem, use 2 ptrs
         # Time complexity: O(n)
         # Space complexity: O(1)
         
-        maxProfit = 0
-        l, r  = 0,1 #left is buy, right is sell
+        # --------------- CLEANER IMPLEMENTATION ---------------
+        min_price = prices[0]
+        max_profit = 0
+        
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        
+        return max_profit
 
-        # keep running the loop, until we reach the last value with r
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxProfit = max(maxProfit, profit)
-            else: #if we find a new low price, set it as the left ptr
-                l=r
-            # always increment the right ptr
-            r+=1
-        return maxProfit
+        # --------------- FIRST IMPLEMENTATION ---------------
+        # # sliding window problem, use 2 ptrs
+        # maxProfit = 0
+        # l, r  = 0,1 #left is buy, right is sell
+        # # keep running the loop, until we reach the last value with r
+        # while r < len(prices):
+        #     if prices[l] < prices[r]:
+        #         profit = prices[r] - prices[l]
+        #         maxProfit = max(maxProfit, profit)
+        #     else: #if we find a new low price, set it as the left ptr
+        #         l=r
+        #     # always increment the right ptr
+        #     r+=1
+        # return maxProfit
