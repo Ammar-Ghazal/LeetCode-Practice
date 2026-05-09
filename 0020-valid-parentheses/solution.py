7984b4780394d2161fb781c1ed71bdc361d050ef
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-09 06:39:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (27.24%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,21 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # O(n) time and space complexity
-        stack = []
-        closeToOpen = {")":"(", "]":"[", "}":"{"}
-        for char in s:
-            # check if it is a closing brace
-            if char in closeToOpen: 
-                if not stack: # if closing brace and stack is empty, return False
-                    return False 
-                # if current closing brace matches last opening brace, remove it from the stack
-                elif closeToOpen[char] == stack[-1]:
-                    stack.pop()
-                else: # if it doesn't match, return false
-                    return False
-            # if not closing brace, must be an opening brace, add to stack
-            else:
-                stack.append(char)
+        # Time complexity: O(n)
+        # Space complexity: O(n)
 
-        # if the stack is still empty, return true, otherwise return false
+        # Use stack to keep track of ordering of opening braces
+        stack = []
+        closeToOpen = {')':'(', ']':'[', '}':'{'}
+
+        for c in s:
+            # If character is closing brace
+            if c in closeToOpen:
+                # if it is a closing brace and the stack is empty it isnt valid
+                if not stack:
+                    return False
+                # if last opening brace matches current closing brace, remove opening brace
+                elif stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                # if the braces doesnt match, it isnt valid
+                else:
+                    return False
+            # if it is an opening brace, add it to the stack
+            else:
+                stack.append(c)
+               
+        # if the stack is empty, return true, otherwise return false
         return not stack
