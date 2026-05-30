bfc91b1c1dee8b452f83617647da85ce1842b663
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-30 16:35:39 +0000

    Sync LeetCode submission Runtime - 39 ms (74.67%), Memory - 28.4 MB (99.36%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,26 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # For both implementations:
         # Time Complexity: O(n)
         # Space Complexity: O(1)
+        lowestBuy = prices[0]
         maxProfit = 0
-        lowestBuyPrice = prices[0]
 
         for p in prices:
-            lowestBuyPrice = min(p, lowestBuyPrice)
-            maxProfit = max(p-lowestBuyPrice, maxProfit)
-        
+            lowestBuy = min(p, lowestBuy)
+            maxProfit = max(p - lowestBuy, maxProfit)
+
         return maxProfit
-
-
-        # First Implementation:
-        # buy, maxProfit = 0, 0
-
-        # for sell in range(len(prices)):
-        #     if prices[sell] > prices[buy]:
-        #         curProfit = prices[sell] - prices[buy]
-        #         maxProfit = max(curProfit, maxProfit)
-        #     else:
-        #         buy = sell
-        
-        # return maxProfit
