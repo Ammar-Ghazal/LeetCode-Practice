ecea238b996139378f2ccaa06d6ddb4935f13950
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-30 17:01:42 +0000

    Sync LeetCode submission Runtime - 11 ms (76.47%), Memory - 19.4 MB (43.89%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,13 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        # Time Complexity: O(n) or O(s+t), but s==t since we return false otherwise
+        # Space Complexity: O(n) or O(s+t)
 
-        countS, countT = {},{}
+        if len(s) != len(t): return False
+        countS, countT = {}, {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        
-        return countS == countT
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
 
+        for char in countS:
+            if countS[char] != countT.get(char, 0):
+                return False
+        
+        return True
