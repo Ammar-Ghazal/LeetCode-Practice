9bd39f131eeb9b38267b3063b8972976b7fbfd96
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-30 21:50:09 +0000

    Sync LeetCode submission Runtime - 27 ms (84.08%), Memory - 31.6 MB (20.27%)

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -2,20 +2,13 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # Time Complexity: O(n)
         # Space Complexity: O(1)
-
-        # initialize one variable to track current sum, and another for overall highest sum
-        curNum = 0
-        maxNum = nums[0]
-
-        # for all numbers, check if they improve the curent sum, if not, reset curNum and keep looking
+        curSum = 0
+        maxSum = nums[0]
+        
         for n in nums:
-            # if curNum is ever negative, set it back to 0 and keep looking for highest sum
-            if curNum < 0:
-                curNum = 0
-            # add current num to latest summation series
-            curNum += n
-            # if current summation is highest, save it
-            maxNum = max(curNum, maxNum)
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSum = max(curSum, maxSum)
 
-        # return highest summation
-        return maxNum
+        return maxSum
