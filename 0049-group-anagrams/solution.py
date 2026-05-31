27724537a0aa0159e796602a0dbfcf0abd1db40d
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-05-31 02:05:11 +0000

    Sync LeetCode submission Runtime - 11 ms (84.31%), Memory - 22.8 MB (34.71%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,41 +1,30 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Time Complexity: O(m*n) -> m is the number of given strings, and n is the average length of each string
-        # Space Complexity: ?
-        
-        # hashmap mapping charCount to list of anagrams
-        anagMap = defaultdict(list)
+        output = defaultdict(list)
         """
-        defaultdict(list) is used because the line:
+        defaultdict(list) is used because of this line:
             anagMap[tuple(count)].append(s)
-        on a regular dict the first time the key is accessed, since the key doesnt exist it, and there is nothing to append to :)
-        defaultdict(list) auto-creates an empty list for any missing key, so we can append directly before creating the key, so it basically does this but prettier:
+        on a regular dict the first time the key is accessed, it still doesnt exist, and will throw an error when you try to append to nothing
+        defaultdict(list) auto-creates an empty list for any missing key, so we can "append" before creating the key, so it basically does this but prettier:
             if key not in d:
                 d[key] = []
             d[key].append(value)
         """
-
+        
         for s in strs:
-            # array to represent a string by the count of each of its letters
-            count = [0] * 26 
+            output[tuple(sorted(s))].append(s)
 
-            # add the number of each letter in its corresponding index (a is 0, b is 1, etc)
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            
-            # since lists cant be keys in python, we use tuple, add the string to the map value
-            anagMap[tuple(count)].append(s)
+        return list(output.values())
 
-        return list(anagMap.values())
         """
-            anagMap = {
+            output = {
                 (1,0,1,0,1,0,...): ["eat", "tea"],
                 (0,1,1,0,0,0,...): ["bat"]
             }
 
-            anagMap.values() → (a special view object, not a list)
+            output.values() → (a special view object, not a list)
                 → dict_values([["eat","tea"], ["bat"]])
 
-            list(anagMap.values()) → (an actual list)
+            list(output.values()) → (an actual list)
                 → [["eat","tea"],["bat"]]
         """
