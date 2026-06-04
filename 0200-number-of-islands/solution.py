292f8a68b1f0be71dde71df931f5f2b36458a5b3
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-06-04 16:45:11 +0000

    Sync LeetCode submission Runtime - 241 ms (66.82%), Memory - 21.5 MB (91.91%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,25 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # Time Complexity: O(n^2)
-        # Space Complexity: O(1)
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        ROWS, COLS = len(grid), len(grid[0])
+        # Time Complexity: O(m*n), where m is the number of rows, and n is the number of columns
+        # Space Complexity: O(m*n), worst case for the max number of recursive calls
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         islands = 0
+        rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c):
-            if (r < 0 or c < 0 or r >= ROWS or
-                c >= COLS or grid[r][c] == "0"
-            ):
+            if (r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0"):
                 return
-
             grid[r][c] = "0"
             for dr, dc in directions:
                 dfs(r + dr, c + dc)
 
-        for r in range(ROWS):
-            for c in range(COLS):
+
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == "1":
                     dfs(r, c)
                     islands += 1
-
+                
         return islands
+
