26afec739ee60bb09b9c495f272a52ce61b55baf
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-07-05 13:47:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (20.55%)

--- 0226-invert-binary-tree/solution.py
+++ 0226-invert-binary-tree/solution.py
@@ -6,22 +6,14 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # Time Complexity: O(n) -> worst case in unbalanced tree
-        # Space Complexity: O(h) -> h (height of tree) is n in the worst case, for the recursive call stack
-        
-        # if it is a leaf node, return None/Null
         if root == None:
-            return
+            return root
 
-        # swap the two children
-        tmp = root.left
+        tempVar = root.left
         root.left = root.right
-        root.right = tmp
+        root.right = tempVar
 
-        # call the invert function on the 2 subtrees
         self.invertTree(root.left)
         self.invertTree(root.right)
 
-        # needed for leetcode grading
         return root
-
