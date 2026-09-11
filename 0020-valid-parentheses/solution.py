2283a751b27b49853e9d57bb59b4145f99f8cda6
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-11 15:48:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (63.22%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # Time Complexity: O(n)
-        # Space Complexity: O(n)
         stack = []
-        closeToOpen = {')':'(', '}':'{', ']':'['}
+        valid = False
+        closeToOpen = {"}":"{", "]":"[", ")":"("}
 
         for b in s:
-            if b in closeToOpen:
-                if stack and closeToOpen[b] == stack[-1]:
-                    stack.pop()
+            if b in closeToOpen: # this is a closing brace
+                if stack and closeToOpen[b] == stack[-1]: # current brace matches last opening brace
+                    stack.pop() # remove the matching closing brace
                 else:
                     return False
-            else:
+            else: # must be an opening brace
                 stack.append(b)
-
-        return not stack
-
         
+        # returns true if the stack is empty, and false otherwise
+        return not stack
