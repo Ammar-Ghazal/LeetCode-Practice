dbf019e8cce1879e7582c7522967b5d83227abd4
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-11 19:46:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20 MB (32.48%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,33 +1,26 @@
 class Solution:
+    # def alphaNum(self, c: str) -> bool:
+    #     return (
+    #         'A' <= c <= 'Z' or
+    #         'a' <= c <= 'z' or
+    #         '0' <= c <= '9'
+    #     )
+
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
+        # l, r = 0, len(s) - 1
 
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while r > l and not self.alphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
+        # while l < r:
+        #     while l < r and not s[l].isalnum():
+        #         l += 1
+        #     while l < r and not s[r].isalnum():
+        #         r -= 1
+        #     if s[l].lower() != s[r].lower():
+        #         return False
+        #     l, r = l + 1, r - 1
+        
+        # return True
 
-        return True
-            
-
-
-        # # -------------- Pythonic Solution --------------
         # Time Complexity: O(n)
         # Space Complexity: O(n)
-        # cleanStr = ""
-
-        # for c in s:
-        #     if c.isalnum():
-        #         cleanStr += c.lower()
-
-        # return cleanStr == cleanStr[::-1]
-            
-    def alphaNum(self, c: str) -> bool:
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
-        
+        clean = "".join(filter(str.isalnum, s)).lower()
+        return clean == clean[::-1]
