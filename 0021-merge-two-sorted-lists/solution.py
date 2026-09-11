d1f8bab1e78977ea790cf375ecea199bbff2bd6c
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-11 16:09:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (73.53%)

--- 0021-merge-two-sorted-lists/solution.py
+++ 0021-merge-two-sorted-lists/solution.py
@@ -5,32 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Time complexity: O(n)
-        # Space complexity: O(1)
+        output = ListNode() # used to be output = None
+        outputHead = output
 
-        # Initialize LL we want to return, and a cur ptr to keep track of current location
-        head = ListNode()
-        cur = head
-
-        # while both lists are non empty:
-        while list1 and list2:
-            # if list1's value is smaller, put it in merged list
-            if list1.val < list2.val:
-                cur.next = list1
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                output.next = list1
+                output = output.next
                 list1 = list1.next
-            # if list2's value is bigger/equal, put it in merged list
             else:
-                cur.next = list2
+                output.next = list2
+                output = output.next
                 list2 = list2.next
-
-            # update current pointer so we dont overwrite what we entered in the next iteration
-            cur = cur.next
         
-        # plug in the rest of the list once the other list runs out
-        if list1:
-            cur.next = list1
-        elif list2:
-            cur.next = list2
-        
-        return head.next
+        if list1 is not None:
+            output.next = list1
+        if list2 is not None:
+            output.next = list2
 
+        return outputHead.next
