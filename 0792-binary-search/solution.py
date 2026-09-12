699f5ff872f0aad2cac46ef50a3de1f1c2d7487b
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-12 05:31:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.5 MB (37.32%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -1,24 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Time Complexity: O(log(n))
-        # Space Complexity: O(1)
+        l, r = 0, len(nums) - 1
 
-        # initialize left, right and middle pointers/indices
-        size = len(nums)
-        l, r = 0, size - 1
-
-        # if at any point l == r, the binary search has failed to locate target
         while l <= r:
-            # update middle every iteration
             m = (l+r)//2
-
-            # update right and left accordingly, and return target if found
-            if nums[m] == target:
+            if target == nums[m]:
                 return m
-            elif nums[m] > target:
-                r = m - 1
-            else:
+            elif target > nums[m]:
                 l = m + 1
+            else:
+                r = m - 1
         
-        # failed to locate target
         return -1
+
