a5ff94fc661edb4f158cc2a2a89cdd200dbfb17e
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-14 16:44:22 +0000

    Sync LeetCode submission Runtime - 1084 ms (14.37%), Memory - 24.1 MB (5.47%)

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -1,32 +1,25 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        # Time Complexity: O(n^2)
-        # Space Complexity: O(1) -> not counting the allTriplets output
-        output = []
-        nums.sort()
+        # Time complexity: O(n^2)
+        # Space complexity: O(n)
+        out = set()
+        used = set()
 
-        # loop for first integer out of the 3 (i, l, r)
-        for i in range(len(nums)):
-            # check that we aren't at the first index, and if it is a duplicate, skip this itr
-            if i>0 and nums[i] == nums[i-1]:
+        for i, numi in enumerate(nums):
+            if numi in used:
                 continue
             
-            # initialize the ptr variables for l and r
-            l, r = i + 1, len(nums) - 1
+            used.add(numi)
+            
+            prev = {}
 
-            # this here is the code taken from 2sum II (version 2)
-            while l < r:
-                threeSum = nums[i] + nums[l] + nums[r]
-                # if sum is less than 0, update left ptr
-                if threeSum < 0:
-                    l += 1
-                elif threeSum > 0:
-                    r -= 1
-                else:
-                    # sum is equal to 0, add it to the output list, and update left ptr (doesnt matter which one we update)
-                    output.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    # if the next number is the same, keep updating the left ptr (to avoid duplicates)
-                    while nums[l] == nums[l-1] and l<r:
-                        l += 1
-        return output
+            for j in range(i + 1, len(nums)):
+                numj = nums[j]
+                numk = -numi - numj
+
+                if numk in prev:
+                    out.add(tuple(sorted((numi, numj, numk))))
+                
+                prev[numj] = j
+        
+        return [list(triplet) for triplet in out]
