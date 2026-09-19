7b34e71d46c2af22fbce7a62616aabaae090aba3
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-19 14:19:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (99.93%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -1,21 +1,19 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums: list[int], target: int) -> int:
+        # Time complexity: O(logn)
+        # Space complexity: O(1)
         l, r = 0, len(nums) - 1
-
+        
         while l <= r:
-            m = (l + r) // 2
-            if nums[m] == target:
-                return m
-            # check if right portion of nums is sorted:
-            if nums[l] <= nums[m]:
-                # check if target is within sorted portion:
-                if target >= nums[l] and target <= nums[m]:
+            m = (l+r)//2
+            if nums[m] == target: return m
+            elif nums[m] >= nums[l]: # left is sorted
+                if nums[l] <= target and target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            # if left portion is unsorted, then right portion must be sorted:
-            else:
-                if target <= nums[r] and target >= nums[m]:
+            else: # right is sorted
+                if nums[m] < target and target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
