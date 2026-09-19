414d8991da34e35848841cb0e6c9f391b819a8a4
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-19 08:53:20 +0000

    Sync LeetCode submission Runtime - 201 ms (99.56%), Memory - 21.8 MB (36.97%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -3,22 +3,23 @@ class Solution:
         # Time Complexity: O(m*n), where m is the number of rows, and n is the number of columns
         # Space Complexity: O(m*n), worst case for the max number of recursive calls
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        cols, rows = len(grid), len(grid[0])
         islands = 0
-        rows, cols = len(grid), len(grid[0])
 
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0"):
+        def dfs(col, row):
+            if col < 0 or row < 0 or col >= cols or row >= rows or grid[col][row] == "0":
                 return
-            grid[r][c] = "0"
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+            grid[col][row] = "0"
+            for c, r in directions:
+                dfs(col + c, row + r)
+            return
+        
 
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    dfs(r, c)
+        for c in range(cols):
+            for r in range(rows):
+                if grid[c][r] == "1":
+                    dfs(c, r)
                     islands += 1
-                
+        
         return islands
 
