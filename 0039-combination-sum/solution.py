e14d34fbd178939f703fd0ea1c28cfbab81da670
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-20 04:51:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (66.16%)

--- 0039-combination-sum/solution.py
+++ 0039-combination-sum/solution.py
@@ -2,25 +2,26 @@ class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         # Time complexity: O(2^(t/m)), t -> target, m -> minimum value in nums
         # Space complexity: O(t/m)
-        res = []
-        nums.sort() # nums is not sorted by leetcode
+        out = []
+        nums.sort()
 
         def dfs(i, cur, total):
             if total == target:
-                res.append(cur.copy())
+                out.append(cur.copy()) 
                 return
-
             for j in range(i, len(nums)):
-                if total + nums[j] > target:
+                if nums[j] + total > target:
                     return
                 cur.append(nums[j])
                 dfs(j, cur, total + nums[j])
                 cur.pop()
-
+        
         dfs(0, [], 0)
-        return res
 
-# Old code:
+        return out
+
+        
+# New code:
 # class Solution:
 #     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
 #         # Time complexity: O(2^(t/m)), t -> target, m -> minimum value in nums
