0ace567486329fb3f261dd8a82e516273aad61ec
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-23 06:29:08 +0000

    Sync LeetCode submission Runtime - 115 ms (97.07%), Memory - 50.6 MB (77.34%)

--- 0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -1,25 +1,14 @@
-# class Solution(object):
-#     def lowestCommonAncestor(self, root, p, q):
-#         if root is None:
-#             return None
-        
-#         left_res = self.lowestCommonAncestor(root.left, p, q)
-#         right_res = self.lowestCommonAncestor(root.right, p, q)
-        
-#         if (left_res and right_res) or (root in [p, q]):
-#             return root
-#         else:
-#             return left_res or right_res
-
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q:
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        # Time complexity: O(n)
+        # Space complexity: O(h), O(logn) best case, O(n) worst case
+        if root is None or root == p or root == q:
             return root
-
+        
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
 
         if left and right:
             return root
-
-        return left or right        
+        
+        return left or right
