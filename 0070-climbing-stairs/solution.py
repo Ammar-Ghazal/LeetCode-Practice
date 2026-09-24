6303c63bb3950c920d0c3ffb0251883d2d29a353
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-24 11:34:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (18.29%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
-        # Time complexity: O(n)
-        # Spacd complexity: O(n) -> depth of recursion tree can go up to n
+        # Time complexity: O(n) -> because of @cache, it doesnt compute n values more than once
+        # Space complexity: O(n) -> depth of recursion tree can go up to n
+
         if n == 1:
             return 1
-        if n == 2:
+        elif n == 2:
             return 2
         else:
-            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+            return (self.climbStairs(n - 1) + self.climbStairs(n - 2))
 
