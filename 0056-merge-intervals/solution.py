7fccad9b7ac8ff29743e6ea36472ce09dda96bbf
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-24 11:28:58 +0000

    Sync LeetCode submission Runtime - 7 ms (73.46%), Memory - 23.2 MB (17.14%)

--- 0056-merge-intervals/solution.py
+++ 0056-merge-intervals/solution.py
@@ -1,12 +1,21 @@
 class Solution:
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-        out = []
         intervals.sort(key = lambda x: x[0])
+        laststart, lastend = intervals[0][0], intervals[0][1]
+        out = []
+
+        # size = len(intervals)
+        # if size == 1 or size == 0: return intervals
 
         for start, end in intervals:
-            if not out or start > out[-1][1]:
-                out.append([start, end])
+            if lastend >= start:
+                lastend = max(end, lastend)
             else:
-                out[-1][1] = max(out[-1][1], end)
+                out.append([laststart, lastend])
+                laststart = start
+                lastend = end
+        
+        out.append([laststart, lastend])
         
         return out
+
