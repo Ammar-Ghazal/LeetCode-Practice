eda38b643d85398eccf6f36437428fdd22c8afbf
Ammar Ghazal <71371993+Ammar-Ghazal@users.noreply.github.com>
2026-09-25 10:22:31 +0000

    Sync LeetCode submission Runtime - 2 ms (99.38%), Memory - 19.2 MB (93.12%)

--- 4418-number-of-intersecting-interval-pairs-i/solution.py
+++ 4418-number-of-intersecting-interval-pairs-i/solution.py
@@ -1,10 +1,20 @@
 class Solution:
     def countIntersectingIntervals(self, intervals: list[list[int]]) -> int:
-        intersects = 0
-        length = len(intervals)
-        for i in range(length):
-            for j in range(i + 1, length):
-                if intervals[i][1] >= intervals [j][0] and intervals[i][0] <= intervals[j][1]:
-                    intersects += 1
+        # Time complexity: O(nlogn)
+        # Space complexity: O(n)
 
-        return intersects
+        # extract the start and end values, and sort them separately
+        starts = sorted(start for start, end in intervals)
+        ends = sorted(end for start, end in intervals)
+
+        out, ended = 0, 0
+
+        for i, start in enumerate(starts):
+            while ends[ended] < start:
+                ended += 1
+            # for each interval, i intervals have started
+            # subtract from those the intervals that have already ended,
+            # and you have the total intersecting intervals for the current interval
+            out += i - ended
+
+        return out
